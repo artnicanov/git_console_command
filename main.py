199fc6a10953670badffda6d00feb7b944876a2e
Artem <artnicanov@gmail.com>
2022-09-25 20:20:19 +0300

    sample of uuid, unix time and base64 encode with Flask

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+from flask import Flask
 import uuid
 import base64
+import time
 
-x_correlation_id = uuid.uuid4()
-print(x_correlation_id)
+app = Flask(__name__)
 
-log_pass = "login:password"
-auth_encoded = (base64.b64encode(log_pass.encode('UTF-8'))).decode('UTF-8')
-print(auth_encoded)
+@app.route("/")
+def cor_id_and_auth():
+
+	x_correlation_id = uuid.uuid4()
+	current_UNIX_time = round(time.time() * 1000)
+
+
+	log_pass = "login:password"
+	auth_encoded = (base64.b64encode(log_pass.encode('UTF-8'))).decode('UTF-8')
+
+	return f"""{x_correlation_id}<br>
+	       <br>{auth_encoded}<br>
+			<br>{current_UNIX_time}"""
+
+app.run(port=5002)
